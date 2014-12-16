b967f812e2977e251f692852282c8bf61e085643
Nicholas McGlincy <nmcglincy@gmail.com>
2014-12-15 19:35:23 -0800

    AOB 20141215

--- new-yeast-gtf.sh
+++ new-yeast-gtf.sh
@@ -9,3 +9,10 @@
 # mTIFs OVERLAP 5P OF ONE ORF
 # mTIFs OVERLAP 3P OF ONE ORF
 # 
+# COPY NICK'S OLD GTF TO MY GITHUB REPO
+# /Users/nmcglincy/Documents/computing/github/nmcglincy/profiling-analysis
+# 
+scp mcglincy@compute1.ingolia-lab.org:/mnt/ingolialab/ingolia/Genomes/Saccharomyces_cerevisiae/YeastGenome/sac_cer_yassour.gtf .
+# 
+# FOR THE mTIFs WE WANT PELECHANO ET AL. (2013) SUPP. DATA 2
+# S2_tcd_mTIFAnno.txt
