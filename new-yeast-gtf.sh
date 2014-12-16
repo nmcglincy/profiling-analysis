b5562c1b38809e063e459cf90f0ca9afd3bc4b44
Nicholas McGlincy <nmcglincy@gmail.com>
2014-12-16 15:22:59 -0800

    new awk script to reformat anno files

--- new-yeast-gtf.sh
+++ new-yeast-gtf.sh
@@ -17,3 +17,27 @@ scp mcglincy@compute1.ingolia-lab.org:/mnt/ingolialab/ingolia/Genomes/Saccharomy
 # 
 # FOR THE mTIFs WE WANT PELECHANO ET AL. (2013) SUPP. DATA 2
 # S2_tcd_mTIFAnno.txt
+# 
+# MANUALLY REMOVED COLUMN HEADERS TO ANOTHER FILE mTIF-anno-headers
+# 
+# GRAB mTIF TYPES INTO SEPARATE FILES USING GREP
+grep -w "Covering one intact ORF" S2_tcd_mTIFAnno.txt > covering-one-intact-orf
+
+awk '{ for (i=1; i<=6; i++) printf("%s%s", $(i), i<6 ? OFS : "\n"); }'
+
+
+
+grep -w "Intergenic transcripts" S2_tcd_mTIFAnno.txt > intergenic-trcpts
+grep -w "Overlap 3' of one ORF" S2_tcd_mTIFAnno.txt > overlap-3p-oneOrf
+grep -w "Overlap 5' of one ORF" S2_tcd_mTIFAnno.txt > overlap-5p-oneOrf
+# 
+# THE INTERGENIC TRANSCRIPTS DON'T SEEM TO HAVE ANY ID, IT WOULD BE USEFUL IF THEY DID.
+
+
+# FUSE INTO FILES BY FLAVOUR
+cat covering-one-intact-orf intergenic-trcpts > mTIF-anno-vanilla
+cat covering-one-intact-orf intergenic-trcpts overlap-5p-oneOrf overlap-3p-oneOrf > mTIF-anno-choco
+# 
+# TODO - LOCATION DATA IS IN THE WRONG FORMAT
+# TODO - HAVE 5PUTR & 3PUTR AS SEPARATE ANNOTATIONS IN GTF
+# 
---
+++ reformater.awk
@@ -0,0 +1,8 @@
+#!/bin/awk -f
+BEGIN {OFS="\t"}
+{
+#	for (i=1; i<=6; i++) printf("%s%s", $(i), i<6 ? OFS : "\n");
+#	for (i=7; i<=(NF-1); i++) printf("%s%s", $(i), i<(NF-1) ? "_" : "\n")
+#    print NF-1
+    print $NF
+}
