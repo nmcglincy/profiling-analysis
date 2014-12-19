4afb3b760e855cd96b9c7ed2455e83b11269980c
Nicholas McGlincy <nmcglincy@gmail.com>
2014-12-19 09:12:44 -0800

    AOB 20141219

--- extra-vanilla-gtf-analysis.R
+++ extra-vanilla-gtf-analysis.R
@@ -14,3 +14,17 @@ xv.data = import.bed(con = "mTIF-exVanilla.bed",
                      asRangedData = FALSE,
                      genome = "saccCer3")
 xv.data
+?split
+foo = split(xv.data, mcols(xv.data)$name)
+foo
+length(mcols(xv.data)$name)
+metadata(xv.data)
+head(foo)
+foo["YAL002W"]
+str(foo)
+length(unique(names(foo)))
+class(foo)
+length(foo)
+seqlengths(foo)
+bar = elementLengths(foo)
+hist(bar)
