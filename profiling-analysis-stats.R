38818162d25adacedcf5385bc4f6f68e831c12ba
Nicholas McGlincy <nmcglincy@gmail.com>
2015-01-14 13:43:49 -0800

    Finished some EDA, making objects for deseq2

--- profiling-analysis-stats.R
+++ profiling-analysis-stats.R
@@ -62,6 +62,115 @@ for (i in qexpr.files) {
                               header = FALSE,
                               sep = "\t",
                               stringsAsFactors = FALSE))
-  names(qexpr.data.l) = c("gene.id", "")     
+  names(qexpr.data.l[[i]]) = c("gene.id", "lnt.quant.reg", "read.count")     
 }
+str(qexpr.data.l)
+names(qexpr.data.l) = c("112A","112B","D19A","D19B")
+qexpr.data.df = ldply(qexpr.data.l)
+# head(qexpr.data.df)
+names(qexpr.data.df)[1] = "lib"
+# 
+summary(qexpr.data.df)
+# WTF, SOMETHING AT -3
+qexpr.data.df[which(qexpr.data.df$lnt.quant.reg < 0),]
+# names(qexpr.data.df)
+# 
+# EDA
+# RAW COUNTS
+# HISTOGRAM BY LIB
+ggplot(qexpr.data.df, aes(x = log2(read.count))) +
+  geom_density(size = 1.25) +
+  facet_wrap(~ lib, ncol = 2) +
+  ylab("Density") +
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 16),
+        axis.title.y = element_text(vjust = 1, size = 16),
+        axis.text.x = element_text(size=14, vjust = 0.5),
+        axis.text.y  = element_text(size=14),
+        plot.title = element_text(size = 18),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 16),
+        strip.text.x = element_text(size = 14),
+        strip.text.y = element_text(size = 14))
+ggsave("read-count-density.png")
+ggplot(qexpr.data.df, aes(x = log2(read.count), colour = lib)) +
+  stat_ecdf() +
+  ylab("ECDF") +
+  scale_colour_manual(name = "Sample",
+                      values = cbbPalette[c(1,2,3,8)]) +
+  guides(colour = guide_legend(override.aes = list(size = 5))) + 
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 16),
+        axis.title.y = element_text(vjust = 1, size = 16),
+        axis.text.x = element_text(size=14, vjust = 0.5),
+        axis.text.y  = element_text(size=14),
+        plot.title = element_text(size = 18),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 16),
+        strip.text.x = element_text(size = 14),
+        strip.text.y = element_text(size = 14))
+ggsave("read-count-ecdf.png")
+# 
+# THE D19 LIBS SEEM TO HAVE LESS READS PER GENE
+# TOTAL READ.COUNT FOR EACH LIBRARY
+library(dplyr)
+write.csv(qexpr.data.df %>%
+            group_by(lib) %>%
+            summarise(total.reads = sum(read.count)),
+          file = "total-reads-summ.csv",
+          quote = FALSE,
+          row.names = FALSE)
+# 
+# GRAPHS BASED ON READ.COUNT NORMALISED FOR TOTAL.READS
+qexpr.data.df.nm = qexpr.data.df %>%
+  group_by(lib) %>%
+  mutate(norm.counts = read.count/sum(read.count))
+# head(qexpr.data.df.nm)
+# 
+ggplot(qexpr.data.df.nm, aes(x = log2(norm.counts))) +
+  geom_density(size = 1.25) +
+  facet_wrap(~ lib, ncol = 2) +
+  ylab("Density") +
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 16),
+        axis.title.y = element_text(vjust = 1, size = 16),
+        axis.text.x = element_text(size=14, vjust = 0.5),
+        axis.text.y  = element_text(size=14),
+        plot.title = element_text(size = 18),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 16),
+        strip.text.x = element_text(size = 14),
+        strip.text.y = element_text(size = 14))
+ggsave("norm-read-count-density.png")
+ggplot(qexpr.data.df.nm, aes(x = log2(norm.counts), colour = lib)) +
+  stat_ecdf() +
+  ylab("ECDF") +
+  scale_colour_manual(name = "Sample",
+                      values = cbbPalette[c(1,2,3,8)]) +
+  guides(colour = guide_legend(override.aes = list(size = 5))) + 
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 16),
+        axis.title.y = element_text(vjust = 1, size = 16),
+        axis.text.x = element_text(size=14, vjust = 0.5),
+        axis.text.y  = element_text(size=14),
+        plot.title = element_text(size = 18),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 16),
+        strip.text.x = element_text(size = 14),
+        strip.text.y = element_text(size = 14))
+ggsave("norm-read-count-ecdf.png")
+# THIS APPEARS TO REMOVE THE DIFFERENCE IN THE DISTRIBUTION BETWEEN THE 112S AND THE D19S
+# SO PROBABLY THE PER GENE DECREASE IS JUST BECAUSE THERE ARE SLIGHTLY LESS READS IN THE D19 LIBS
+# 
+# DESEQ ANALYSIS
+# CREATING A MATRIX OF COUNT DATA
+ls()
+str(qexpr.data.l)
+# 
+# CHECKING THE LENGTH, ORDER AND COMPOSITION OF THE GENE.ID COLUMNS IS THE SAME
+lapply(qexpr.data.l, nrow)
+for (i in 1:length(qexpr.data.l)) {
+  print(identical(qexpr.data.l[[1]]$gene.id, qexpr.data.l[[i]]$gene.id))
+}
+
 
